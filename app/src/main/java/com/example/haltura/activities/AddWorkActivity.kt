7d214bb80dda7ed3d2e8806e5669012c391ea2f2
omer <11omerzz1@gmail.com>
2022-03-23 15:41:04 +0200

    layouts

---
+++ app/src/main/java/com/example/haltura/activities/AddWorkActivity.kt
@@ -0,0 +1,11 @@
+package com.example.haltura.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddWorkActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_work)
+    }
+}
