051f9e1f54ff2c7ba83570eeddbc37ba2bc521ef
omer <11omerzz1@gmail.com>
2022-03-31 16:25:44 +0300

    firebase

--- app/src/main/java/com/example/haltura/activities/LoginActivity.kt
+++ app/src/main/java/com/example/haltura/activities/LoginActivity.kt
@@ -2,7 +2,6 @@ package com.example.haltura.activities
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -22,42 +21,42 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        helper = UserOpenHelper(this)
-        etUserName = findViewById<View>(R.id.etUserName) as EditText
-        etPassword = findViewById<View>(R.id.etPassword) as EditText
-        login = findViewById<View>(R.id.btn_login) as Button
+        helper = UserOpenHelper()//this)
+        etUserName = findViewById<View>(R.id.et_UserName) as EditText
+        etPassword = findViewById<View>(R.id.et_Password) as EditText
+        login = findViewById<View>(R.id.btn_Login) as Button
     }
 
-    fun login(view: View)
-    {
-        if (helper.checkUser(etUserName.text.toString(), etPassword.text.toString())) {
-            UserData.currentUser = helper.getUserByUsername(etUserName.text.toString())
-            if (UserData.currentUser != null) {
-                //                     Toast.makeText(LogInActivity.this, UserData.currentUser.toString(), Toast.LENGTH_SHORT).show();
-                Toast.makeText(this, "login complete!", Toast.LENGTH_SHORT).show()
-                val mainIntent: Intent
-                mainIntent = if (UserData.isAdmin()) {
-                    Intent(this, LoginActivity::class.java)
-                } else {
-                    Intent(this, MainActivity::class.java)
-                }
-                startActivity(mainIntent)
-                finish()
-            }
-        } else {
-            Toast.makeText(
-                this,
-                "user or password incorrect!",
-                Toast.LENGTH_SHORT
-            ).show()
-        }
-    }
-    fun register(view: View)
-    {
-        startActivity(Intent(this, SignUpActivity::class.java))
-    }
-    fun forgotPassword(view: View)
-    {
-        startActivity(Intent(this, ForgotYourPasswordActivity::class.java))
-    }
+//    fun login(view: View)
+//    {
+//        if (helper.checkUser(etUserName.text.toString(), etPassword.text.toString())) {
+//            UserData.currentUser = helper.getUserByUsername(etUserName.text.toString())
+//            if (UserData.currentUser != null) {
+//                //                     Toast.makeText(LogInActivity.this, UserData.currentUser.toString(), Toast.LENGTH_SHORT).show();
+//                Toast.makeText(this, "login complete!", Toast.LENGTH_SHORT).show()
+//                val mainIntent: Intent
+//                mainIntent = if (UserData.isAdmin()) {
+//                    Intent(this, LoginActivity::class.java)
+//                } else {
+//                    Intent(this, MainActivity::class.java)
+//                }
+//                startActivity(mainIntent)
+//                finish()
+//            }
+//        } else {
+//            Toast.makeText(
+//                this,
+//                "user or password incorrect!",
+//                Toast.LENGTH_SHORT
+//            ).show()
+//        }
+//    }
+//    fun register(view: View)
+//    {
+//        startActivity(Intent(this, SignUpActivity::class.java))
+//    }
+//    fun forgotPassword(view: View)
+//    {
+//        startActivity(Intent(this, ForgotYourPasswordActivity::class.java))
+//    }
 }
