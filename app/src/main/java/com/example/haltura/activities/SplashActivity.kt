c731294365c9c62b2dd1d1f74e3068af848713c9
liran <liranusa6@gmail.com>
2022-06-11 12:18:28 +0300

    registration works

--- app/src/main/java/com/example/haltura/activities/SplashActivity.kt
+++ app/src/main/java/com/example/haltura/activities/SplashActivity.kt
@@ -1,15 +1,17 @@
 package com.example.haltura.activities
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.haltura.Sql.UserOpenHelper
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
     private lateinit var helper: UserOpenHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        helper = UserOpenHelper(this)
+//        helper = UserOpenHelper(this)
         if(!(helper.isUserLoggedIn())){
             startActivity(Intent(this, LoginActivity::class.java)) //todo: change
         }
